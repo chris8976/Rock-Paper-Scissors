8b1fc9e973233784a717b3ec03298f70b945c25e
Christopher Dominguez <Neymar15216@gmail.com>
2022-12-22 14:18:06 -0500

    aaaa

--- Rock Paper Scissors/ViewController.swift
+++ Rock Paper Scissors/ViewController.swift
@@ -6,14 +6,50 @@
 //
 
 import UIKit
-
+ 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateInterface(uh:.start)
     }
+    @IBOutlet var ComputerSign: UILabel!
+       @IBOutlet var GameStatus: UILabel!
+       @IBOutlet var UserRock: UIButton!
+       @IBOutlet var UserPaper: UIButton!
+       @IBOutlet var UserScissors: UIButton!
+       @IBOutlet var PlayAgain: UIButton!
+       
+       
+       @IBAction func UsersRock(_ sender: Any) {
+           play(ha: .rock)
+       }
+       @IBAction func UsersPaper(_ sender: Any) {
+           play(ha: .paper)
+       }
+       @IBAction func UsersScissors(_ sender: Any) {
+           play(ha: .scissors)
+       }
+       @IBAction func paButton(_ sender: Any) {
+           updateInterface(uh: .start)
+       }
+       
+       func updateInterface(uh : GameState) {
+           if uh == .start {
+               ComputerSign.text = "🤖"
+               GameStatus.text = "Make a move"
+               PlayAgain.isHidden = true
+               UserRock.isEnabled = true
+               UserPaper.isEnabled = true
+               UserScissors.isEnabled = true
+               view.backgroundColor =  colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+               
+           }
 
+       }
+    
+    
 
 }
 
---
+++ Rock Paper Scissors/sign.swift
@@ -0,0 +1,11 @@
+//
+//  sign.swift
+//  Rock Paper Scissors
+//
+//  Created by 6f on 12/20/22.
+//
+
+import Foundation
+ 
+
+    
